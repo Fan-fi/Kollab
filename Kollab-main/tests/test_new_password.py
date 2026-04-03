e957636c1ae96711a19aedc987122c7007edde0b
Fan-fi <naboychenko1201@gmail.com>
2026-04-03 17:21:13 +0300

    Add files via upload

---
+++ Kollab-main/tests/test_new_password.py
@@ -0,0 +1,24 @@
+import string
+from password.new_password import generate_password
+
+def test_password_characters():
+    """Тест, что при генерации используются только допустимые символы"""
+    valid_characters = string.ascii_letters + string.digits + string.punctuation
+    password = generate_password(100)  # Генерируем длинный пароль для более надежной проверки
+    for char in password:
+        assert char in valid_characters
+def test_dlina():
+    for 1 in (1 ,20):
+        assert len(generate_password(1))==1
+def test_2password():
+    pas1=generate_password (8)
+    pas2=generate_password (8)
+    assert pas1!=pas2
+
+"""
+Допиши еще один тест из предложенных. Или придумай свой.
+Если сможешь написать больше, то будет круто!
+
+Тест, что длина пароля соответствует заданной
+Тест, что два сгенерированных подряд пароля различаются
+"""
